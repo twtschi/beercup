872035bc28945f2b71d991d9918ef46cbd90f3a3
TingKai Wu(吳庭愷) <TingKai_Wu@HTC.COM>
2016-05-24 12:05:09 +0800

    Add react-hot-loader for easier to develop

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-var config = {
-   entry: './main.js',
-	
-   output: {
-      path:'./',
-      filename: 'index.js',
-   },
-	
-   devServer: {
-      inline: true,
-      port: 3000,
-   },
-	
-   module: {
-      loaders: [ {
-         test: /\.jsx?$/,
-         exclude: /node_modules/,
-         loader: 'babel',
-			
-         query: {
-            presets: ['es2015', 'react']
-         }
-      }]
-   }
-	
-}
+var webpack = require('webpack');
 
-module.exports = config;
+module.exports = {
+  entry: [
+    "webpack-dev-server/client?http://10.8.53.71:3000",
+    "webpack/hot/only-dev-server",
+    "index.html"
+  ],
+  output: {
+    path: __dirname,
+    filename: "build/js/bundle.js",
+    publicPath: "/build"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'react-hot!jsx-loader?harmony'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
