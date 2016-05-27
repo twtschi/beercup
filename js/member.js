bb0aa0a68afd47bf00c6e9713c523128ed4649a1
vincent_wu <vincent_wu@htc.com>
2016-05-27 15:49:15 +0800

    Update login page template

---
+++ js/member.js
@@ -0,0 +1,25 @@
+//import React from 'react';
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class FacebookLogin extends React.Component {
+
+  static propTypes = {
+    callback: PropTypes.func.isRequired,
+    appId: PropTypes.string.isRequired,
+    xfbml: PropTypes.bool,
+    cookie: PropTypes.bool,
+    scope: PropTypes.string,
+    textButton: PropTypes.string,
+    autoLoad: PropTypes.bool,
+    size: PropTypes.string,
+    fields: PropTypes.string,
+    cssClass: PropTypes.string,
+    version: PropTypes.string,
+    icon: PropTypes.string,
+    language: PropTypes.string,
+  };
+  
+  
+}
